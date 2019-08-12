5f921b9f8c3e46bc519065f7cfbc532b70f6676a
Michael <mqngo21@gmail.com>
2019-08-12 11:45:05 -0700

    init commit

---
+++ Algos/BinarySearch.js
@@ -0,0 +1,7 @@
+// TODO - Given a sorted array of integers, return the index of the given key. Return -1 if not found.
+// Description: Binary search compares the target value to the middle element of the array. (Log search)
+
+// Inputs: sorted integers [], key
+// Outputs: index of given key or -1 if not found
+// Constraints:
+// Edge Cases:
---
+++ Algos/BinarySearch.md
@@ -0,0 +1,1 @@
+![alt BinarySearch](BinarySearch.png)
